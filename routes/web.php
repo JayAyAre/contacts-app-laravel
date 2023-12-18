777fd9a440c9aa0a6744565c32aa68e13ce9e1b3
JayAyAre <sandyjordi9@gmail.com>
2023-12-18 17:51:28 +0100

    Create contact with sql

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Models\Contact;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,20 +27,20 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/contact', fn() => Response::view('contact'));
+Route::get('/contact/create', [App\Http\Controllers\ContactController::class, 'create'])
+    ->name('contact.create');
+Route::post('/contact', [App\Http\Controllers\ContactController::class, 'store'])
+    ->name('contact.store');
+
+/*Route::get('/contact', fn() => Response::view('contact'));
 
 Route::post('/contact', function (Request $request) {
-    return Response::json([
-        'success' => true
-    ]);
-});
+    $data = $request->all();
+    Contact::create($data);
+  $contact = new \App\Models\Contact();
+    $contact->name= $data['name'];
+    $contact->phone_number= $data['phone_number'];
+    $contact->save();
+    return "Contact Saved";
+});*/
 
-Route::get('/change-password', fn() => Response::view('change-password'));
-
-Route::post('/change-password', function (Request $request) {
-       if (auth()->check()){
-           return response("Password Changed to: {$request->get('password')}", 200);
-       }else {
-           return response("Not Authorized,401");
-   }
-});
